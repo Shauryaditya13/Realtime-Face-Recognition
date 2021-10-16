691e832c5375d7913804004b79d0796484642721
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-10-16 18:25:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,26 @@
-Webcam.set({
-    width:360,
-    height:300,
-    image_format:'png',
-    png_quality:90
-   });
-   
-   camera=document.getElementById("camera");
-   Webcam.attach("camera");
-   
-   function TakeImage() {
-    Webcam.snap(function(data_url){
-    document.getElementById("Result").innerHTML="<img id='capturedimage' src='"+data_url+"'>";
-    });
-   }
-   console.log("ml5version",ml5.version);
-   imageclassifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/agSFt2ny9/model.json",modelloaded);
-
-   function modelloaded() {
-    console.log("modelloaded");
+function preload() {
 }
-
-function IdentifyImage() {
-    img=document.getElementById("capturedimage");
-    imageclassifier.classify(img,getresult);
+function setup() {
+    canvas = createCanvas(600, 370);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/agSFt2ny9/model.json", modelLoaded);
 }
-
-function getresult(error,results) {
-if(error) {
-    console.log(error);
+function draw() {
+    image(video, 0, 0, 600, 370);
+    classifier.classify(video, getResults);
 }
-else{
-    console.log(results);
-    document.getElementById("Face_Name").innerHTML=results[0].label;
-    document.getElementById("Face_Accuracy").innerHTML=results[0].confidence.toFixed(3);
+function modelLoaded() {
+    console.log("model loaded!");
 }
+function getResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+        document.getElementById("accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
